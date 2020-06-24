dcb476c35b21349ecb73cdf5a73019cbb0991944
Nahuel Chinchilla <nahuelchinchilla@MacBook-Pro-de-Nahuel.local>
2020-06-24 20:08:02 -0300

    fix swagger

--- src/main/java/com/tecso/config/SwaggerConfig.java
+++ src/main/java/com/tecso/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.tecso.controller "))
+                .apis(RequestHandlerSelectors.basePackage("com.tecso.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(getApiInfo());
